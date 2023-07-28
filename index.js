7600d6a7d905e259f054bfac5f4941ab49eb6fd0
Rigrios <igoraleksandrov@MacBook-Pro-Igor.local>
2023-07-28 11:31:20 +0700

    Final1.0

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.querySelector('.byrger').addEventListener('click', function () {
     document.querySelector('.nav').classList.toggle('active')
+    document.body.classList.toggle('lock')
+})
+
+
+document.querySelector('.nav').addEventListener('click', e => {
+    document.querySelector('.nav').classList.remove('active')
 })
